2e5b9ccdd14a2cacca1496aa61c5fe97a4396237
Sweety <sweetysingh91221@gmail.com>
2024-12-19 19:39:07 +0530

    Captain Home UI created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,9 @@ import UserProtectWrapper from './pages/UserProtectWrapper'
 import UserLogout from './pages/UserLogout'
 import CaptainHome from './pages/CaptainHome'
 import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
+import Riding from './pages/Riding'
+import CaptainRiding from './pages/CaptainRiding'
+import 'remixicon/fonts/remixicon.css'
 
 const App = () => {
   return (
@@ -17,6 +20,8 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Start/>} />
       <Route path='/login' element={<UserLogin/>} />
+      <Route path='/riding' element={<Riding/>} />
+      <Route path='/captain-riding' element={<CaptainRiding/>} />
       <Route path='/signup' element={<UserSignup/>} />
       <Route path='/captain-login' element={<Captainlogin/>} />
       <Route path='/captain-signup' element={<CaptainSignup/>} />
