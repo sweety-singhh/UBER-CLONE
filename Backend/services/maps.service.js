f0c02de53c27527904788a28c5f039426efd4059
Sweety <sweetysingh91221@gmail.com>
2024-12-20 18:49:10 +0530

    Google Maps API Enabled

--- Backend/services/maps.service.js
+++ Backend/services/maps.service.js
@@ -3,16 +3,15 @@ const axios = require('axios');
 module.exports.getAddressCoordinate = async (address) => {
     const apiKey = process.env.GOOGLE_MAPS_API;
     //const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
-   console.log(address)
-   console.log(apiKey)
+ 
 
     const url = `https://us1.locationiq.com/v1/search.php?key=${apiKey}&q=${address}&format=json`;
    
 
     try {
         const response = await axios.get(url);
-        if (response.data.status === 'OK') {
-            const location = response.data.results[ 0 ].geometry.location;
+        if (response.data && response.data.length > 0) {
+            const location = response.data[0];
             return {
                 ltd: location.lat,
                 lng: location.lon
