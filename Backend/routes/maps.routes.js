d6bea8e2d8ce26e3cabd75c83055922c0aa78a1f
Sweety <sweetysingh91221@gmail.com>
2024-12-20 14:11:45 +0530

    Captain Home UI Done

---
+++ Backend/routes/maps.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middleware/auth.middleware');
+const mapController = require('../controllers/map.controller');
+const { query } = require('express-validator');
+
+
+router.get('/get-coordinates',
+    query('address').isString().isLength({ min: 3 }),
+    authMiddleware.authUser,
+    mapController.getCoordinates
+);
+
+
+module.exports = router;
