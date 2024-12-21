74f96013c661b9bb8c81d5efcdc4d96799b27986
Sweety <sweetysingh91221@gmail.com>
2024-12-21 11:52:26 +0530

    Creating rides in the backend

--- Backend/app.js
+++ Backend/app.js
@@ -9,6 +9,7 @@ const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
 const mapsRoutes= require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
 
 
 connectToDb();
@@ -29,6 +30,7 @@ app.get('/', (req, res) =>{
 app.use('/users',userRoutes);
 app.use('/captains', captainRoutes);
 app.use('/maps',mapsRoutes);
+app.use('/rides', rideRoutes);
 
 
 module.exports =app;
